e24cf3b7de772ae0d70130b5636add47bde232d6
Felipe Neiva <felipeneivals@gmail.com>
2023-08-15 12:49:45 -0300

    fix(account_recovery): changed email example

--- backend/src/schemas/recovery.py
+++ backend/src/schemas/recovery.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, EmailStr
 
 class AccountRecovery(BaseModel): # modelando a recuperação de senha
-    email: EmailStr = "user@email.com"
+    email: EmailStr = "user@example.com"
     new_password: str = "nova senha"
     confirm_password: str = "confirmar senha"
