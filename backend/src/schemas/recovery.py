c3813432d072bacd2596be3af28dd78c7527c0a1
Felipe Neiva <felipeneivals@gmail.com>
2023-08-15 12:54:28 -0300

    refactor(account_recovery): update account recovery body

--- backend/src/schemas/recovery.py
+++ backend/src/schemas/recovery.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, EmailStr
 
 class AccountRecovery(BaseModel): # modelando a recuperação de senha
-    email: EmailStr = "user@example.com"
-    new_password: str = "nova senha"
-    confirm_password: str = "confirmar senha"
+    email: EmailStr = "user@email.com"
+    new_password: str = "new password"
+    confirm_password: str = "confirm password"
