1b399af9251befd896718572903811c950dd8e31
Hugo Marques <43884516+hggmarks@users.noreply.github.com>
2023-08-14 22:12:20 -0300

    test: TestClient for unit testing

--- backend/src/conftest.py
+++ backend/src/conftest.py
@@ -1,21 +1,35 @@
+from typing import Generator
+
 import pytest
 from fastapi.testclient import TestClient
-from typing import Generator
+
 from src.main import app
 
-@pytest.fixture(scope="function")
+
+@pytest.fixture()
+def unit_test_client():
+    """
+    Create a unit test client for the FastAPI app.
+    """
+    return TestClient(app)
+
+
+@pytest.fixture(scope='function')
 def client() -> Generator:
     """
-    Create a test client for the FastAPI app.
+    Create a bdd test client foth the FastAPI app.
     """
+
     with TestClient(app) as c:
         yield c
+
 
 @pytest.fixture
 def context():
     """
     Variable to store context data between steps.
-    Note: remember to always return the context variable at the end of the each steps.
+    Note: remember to always return the context
+    variable at the end of the each steps.
     """
     b = {}
     yield b
