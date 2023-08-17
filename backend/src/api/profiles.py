a76d2e80a6b4b288392c8f876c93b78060b6ee9d
Frederick <antvenomxd72@gmail.com>
2023-08-16 23:43:02 -0300

    feat(profiles): add get and profiles routes

--- backend/src/api/profiles.py
+++ backend/src/api/profiles.py
@@ -45,3 +45,49 @@ async def create_profile(
     return profile_with_id
 
 
+# retorna todos os profiles de um usuário
+@router.get(
+    '/', status_code=200, response_model=ProfileList, tags=['profiles']
+)
+async def get_profiles(
+    current_user: Annotated[UserDB, Depends(get_logged_user)]
+):
+    profile_list = get_profiles_by_id(current_user.id) # retorna uma lista com todos os profiles do usuário
+
+    print(profile_list)
+    return {'profiles': profile_list}
+
+
+# retorna um perfil especifico de um usuario
+@router.get(
+    '/{id}', status_code=200, response_model=ProfileDB, tags=['profiles']
+)
+async def get_profiles(
+    id:int , current_user: Annotated[UserDB, Depends(get_logged_user)]
+):
+    profile_list = get_profiles_by_id(current_user.id) # retorna uma lista com todos os profiles do usuário
+
+    # garantir que esse profile existe
+    if id < 1 and id > len(profile_list):
+        raise HTTPException(status_code=404, detail='Profile não encontrado')
+    
+    profile = profile_list[id-1] # seleciona o profile com o id passado
+
+    return profile
+
+
+# deleta um profile especificado
+@router.delete(
+    '/{id}', status_code=200, response_model=ProfileDB, tags=['profiles']
+)
+async def remove_profile(
+    id:int , current_user: Annotated[UserDB, Depends(get_logged_user)]
+):
+    profile_list = get_profiles_by_id(current_user.id) # retorna uma lista com todos os profiles do usuário
+
+    # garantir que esse profile existe
+    if id < 1 and id > len(profile_list):
+        raise HTTPException(status_code=404, detail='Profile não encontrado')
+    
+    return database_profiles.pop(id-1) # remove o profile com o id passado
+
