ae09095ade15c38d3141b760ce357543ff7d3f1a
Felipe Neiva <felipeneivals@gmail.com>
2023-08-16 23:20:34 -0300

    feat(account_recovery): create test_account_recovery.py

--- backend/src/schemas/recovery.py
+++ backend/src/schemas/recovery.py
@@ -4,3 +4,8 @@ class AccountRecovery(BaseModel): # modelando a recuperação de senha
     email: EmailStr = "user@email.com"
     new_password: str = "new password"
     confirm_password: str = "confirm password"
+
+class PasswordChangeResponse(BaseModel):
+    email: EmailStr
+    new_password: str
+    confirm_password: str
