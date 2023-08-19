85bafa1b9041a43f0d27a28ab227b2347182f97e
Frederick <antvenomxd72@gmail.com>
2023-08-18 21:25:00 -0300

    feat(video): add video class for testing

---
+++ backend/src/schemas/videos.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+class VideoModel(BaseModel):
+    id : int
+    name : str
+    duration : str
+    gender : str
+    year : int
+    classification : int
+    sinopse : str
+
+class VideoList(BaseModel):
+    videos : list[VideoModel]
+
+class MovieModel(VideoModel):
+    imdb : float
+
+class MovieList(BaseModel):
+    movies : list[MovieModel]
+
+class SerieVideo(VideoModel):
+    episode : int
+
+class SerieSeason(BaseModel):
+    season : int
+    season_episodes : list[SerieVideo]
+
+class SerieModel(BaseModel):
+    name : str
+    serie_seasons : list[SerieSeason]
