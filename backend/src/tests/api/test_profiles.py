de2e017a1630632581833006366f70e45d4ac93a
Frederick <antvenomxd72@gmail.com>
2023-08-21 18:34:47 -0300

    test(profiles): add unit test for get profiles and get profile (by id)

--- backend/src/tests/api/test_profiles.py
+++ backend/src/tests/api/test_profiles.py
@@ -10,8 +10,6 @@ def test_create_profile(client: unit_test_client):
         }
     )
 
-    print(login_response.json())
-
     access_token = login_response.json()['access_token']
     
     response = client.post(
@@ -25,6 +23,62 @@ def test_create_profile(client: unit_test_client):
     )
 
     assert response.status_code == 201
+    assert response.json() == {
+        'nickname': 'fred',
+        'pg': 16,
+        'language': 'pt-br',
+        'id_profile': 1,
+        'id_user': 1,
+    }
+
+
+def test_get_profiles(client: unit_test_client):
+    login_response = client.post(
+        '/login/',
+        data={
+            'username': 'fal5@examle.com',
+            'password': 'string',
+        }
+    )
+
+    access_token = login_response.json()['access_token']
+
+    response = client.get(
+        '/profiles/',
+        headers={'Authorization': f'Bearer {access_token}'},
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {
+        'profiles': [
+            {
+                'nickname': 'fred',
+                'pg': 16,
+                'language': 'pt-br',
+                'id_profile': 1,
+                'id_user': 1,
+            }
+        ]
+    }
+
+
+def test_get_profile(client: unit_test_client):
+    login_response = client.post(
+        '/login/',
+        data={
+            'username': 'fal5@examle.com',
+            'password': 'string',
+        }
+    )
+
+    access_token = login_response.json()['access_token']
+
+    response = client.get(
+        '/profiles/1',
+        headers={'Authorization': f'Bearer {access_token}'},
+    )
+
+    assert response.status_code == 200
     assert response.json() == {
         'nickname': 'fred',
         'pg': 16,
