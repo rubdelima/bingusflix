198dd1bf0542c84d1a0144b9bb90616564aba3f8
Frederick <antvenomxd72@gmail.com>
2023-08-21 19:34:22 -0300

    test(favorites): add second post test

--- backend/src/tests/api/test_favorites.py
+++ backend/src/tests/api/test_favorites.py
@@ -56,3 +56,29 @@ def test_add_favorite(client: unit_test_client):
     }
 
 
+def test_add_favorite2(client: unit_test_client):
+    login_response = client.post(
+        '/login/',
+        data={
+            'username': 'fal5@examle.com',
+            'password': 'string',
+        }
+    )
+
+    access_token = login_response.json()['access_token']
+    
+    response = client.post(
+        '/favorites/',
+        json={
+            'id_video': 2,
+        },
+        headers={'Authorization': f'Bearer {access_token}'},
+    )
+
+    assert response.status_code == 201
+    assert response.json() == {
+        'id_video': 2,
+        'id_user': 1,
+        'id_profile': 1,
+        'id_favorite': 2,
+    }
