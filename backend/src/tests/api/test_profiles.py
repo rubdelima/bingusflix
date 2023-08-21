4b98cb47b9b1958ca3a4d61f33f8b96c0619f4cf
Frederick <antvenomxd72@gmail.com>
2023-08-21 18:41:22 -0300

    test(profiles): add unit test for delete profile

--- backend/src/tests/api/test_profiles.py
+++ backend/src/tests/api/test_profiles.py
@@ -86,3 +86,29 @@ def test_get_profile(client: unit_test_client):
         'id_profile': 1,
         'id_user': 1,
     }
+
+
+def test_delete_profile(client: unit_test_client):
+    login_response = client.post(
+        '/login/',
+        data={
+            'username': 'fal5@examle.com',
+            'password': 'string',
+        }
+    )
+
+    access_token = login_response.json()['access_token']
+
+    response = client.get(
+        '/profiles/1',
+        headers={'Authorization': f'Bearer {access_token}'},
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {
+        'nickname': 'fred',
+        'pg': 16,
+        'language': 'pt-br',
+        'id_profile': 1,
+        'id_user': 1,
+    }
