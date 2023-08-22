385c477a051e539890b13e9a927c75e370a1e4d8
Frederick <antvenomxd72@gmail.com>
2023-08-22 05:38:02 -0300

    test(favorites): add service test (successful list favorites)

--- backend/src/tests/api/features/favorites.feature
+++ backend/src/tests/api/features/favorites.feature
@@ -14,9 +14,9 @@ Feature: Favorites
         Then o status da resposta deve ser "200"
         And esse usuário possui "1" favoritos
     
-    # Scenario: Mostra todos os favorites de um usuário
-    #     Given um usuário com id "1", e-mail "fal5@gmail.com", senha "senha123" está logado no sistema com o perfil com id "1"
-    #     And esse usuário possui "2" favoritos: favorito com id "1" e vídeo "20" e favorito com id "2" e vídeo "12"
-    #     When o usuário envia uma requisição GET para "/favorites"
-    #     Then o status da resposta deve ser "200"
-    #     And o JSON da resposta deve conter o id do vídeo "20", o id do usuário "1", o id do perfil "1" e o id do favorito "1" e o id do vídeo "12", o id do usuário "1", o id do perfil "1" e o id do favorito "2"
+    Scenario: Mostra todos os favorites de um usuário
+        Given um usuário com id "1", e-mail "fal5@gmail.com", senha "senha123" está logado no sistema com o perfil com id "1"
+        And esse usuário possui "2" favoritos: favorito com id "1" e vídeo "20" e favorito com id "2" e vídeo "12"
+        When o usuário envia uma requisição GET para "/favorites"
+        Then o status da resposta deve ser "200"
+        And o JSON da resposta deve conter: o id do vídeo "20" e o id do favorito "1", com id do usuário "1" e o id do perfil "1", e o id do vídeo "12" e id do favorito "2", com id do usuário "1" e o id do perfil "1"
--- backend/src/tests/api/step_definitions/test_favorites.py
+++ backend/src/tests/api/step_definitions/test_favorites.py
@@ -165,3 +165,87 @@ def check_favorite_removal_response_json(context, n_favorites: str):
     assert len(db_f) == int(n_favorites)
 
     return context
+
+
+@scenario(
+    scenario_name="Mostra todos os favorites de um usuário",
+    feature_name="../features/favorites.feature"
+)
+def test_show_all_favorites():
+    """Test the show all favorites"""
+
+@given(
+    parsers.cfparse(
+        'um usuário com id "{user_id}", e-mail "{user_email}", senha "{user_password}" está logado no sistema com o perfil com id "{profile_id}"')
+)
+def mock_user_in_database(client, context, user_id: str, user_email: str, user_password: str, profile_id: str):
+    client, context["access_token"] = login_user_profile(client, user_email, user_password, user_id, profile_id)
+
+    context["user_id"] = user_id
+    context["profile_id"] = profile_id
+
+    return context
+
+@given(
+    parsers.cfparse('esse usuário possui "{n_favorites}" favoritos: favorito com id "{id_favorite1}" e vídeo "{id_video1}" e favorito com id "{id_favorite2}" e vídeo "{id_video2}"')
+)
+def mock_favorites_in_database(context, n_favorites: str, id_favorite1: str, id_video1: str, id_favorite2: str, id_video2: str):
+    db_f.clear()
+    db_f.append(FavoriteDB(
+        id_video= int(id_video1),
+        id_user= int(context["user_id"]),
+        id_profile= int(context["profile_id"]),
+        id_favorite= int(id_favorite1)
+    ))
+    db_f.append(FavoriteDB(
+        id_video= int(id_video2),
+        id_user= int(context["user_id"]),
+        id_profile= int(context["profile_id"]),
+        id_favorite= int(id_favorite2)
+    ))
+
+    return context
+
+@when(
+    parsers.cfparse('o usuário envia uma requisição GET para "{favorites_url}"'), target_fixture="context"
+)
+def send_favorite_show_all_request(client, context, favorites_url: str):
+    response = client.get(
+        favorites_url, 
+        headers={'Authorization': f'Bearer {context["access_token"]}'}, 
+    )
+    context["response"] = response
+    return context
+
+@then(
+    parsers.cfparse('o status da resposta deve ser "{status_code}"'), target_fixture="context"
+)
+def check_favorite_show_all_response_status_code(context, status_code: str):
+    assert context["response"].status_code == int(status_code)
+
+    return context
+
+@then(
+    parsers.cfparse('o JSON da resposta deve conter: o id do vídeo "{video1_id}" e o id do favorito "{favorite1_id}", com id do usuário "{user1_id}" e o id do perfil "{profile1_id}", e o id do vídeo "{video2_id}" e id do favorito "{favorite2_id}", com id do usuário "{user2_id}" e o id do perfil "{profile2_id}"'), target_fixture="context"
+)
+def check_favorite_show_all_response_json(context, video1_id: str, favorite1_id: str, user1_id: str, profile1_id: str, video2_id: str, favorite2_id: str, user2_id: str, profile2_id: str):
+    expected_response = {
+        'favorites': [
+            {
+                'id_video': int(video1_id),
+                'id_user': int(user1_id),
+                'id_profile': int(profile1_id),
+                'id_favorite': int(favorite1_id),
+            },
+            {
+                'id_video': int(video2_id),
+                'id_user': int(user2_id),
+                'id_profile': int(profile2_id),
+                'id_favorite': int(favorite2_id),
+            },
+        ]
+    }
+
+    assert context["response"].json() == expected_response
+
+    return context
