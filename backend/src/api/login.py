cff9471908c6d6dd1cf071dba66648a51c4836d1
Felipe Neiva <felipeneivals@gmail.com>
2023-09-08 11:18:45 -0300

    refactor(login): extrated the function to search for a user by his email

--- backend/src/api/login.py
+++ backend/src/api/login.py
@@ -9,6 +9,12 @@ router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login") # esquema de autenticação por token
 
+def search_user(email: str): # função que procura um usuário na base de dados baseado no email
+    for user in database:
+        if user.email == email:
+            return user
+    return None
+
 async def get_logged_user(token: Annotated[str, Depends(oauth2_scheme)]):
     for user in database: # procura o dicionário do usuário na base de dados baseado no username digitado
         if user.id == int(token):
@@ -18,14 +24,13 @@ async def get_logged_user(token: Annotated[str, Depends(oauth2_scheme)]):
         "/", status_code=200  , response_model=TokenResponse, tags=["login"]
 ) 
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    for user in database: # procura um usuário com email e senha iguais ao do formulário
-        if user.email == form_data.username:
-            if user.passwd == form_data.password:
-                return TokenResponse(access_token=str(user.id), token_type="bearer") # se encontrar, ele recebe um token equivalente ao seu ID
-            else:
-                raise HTTPException(status_code=401, detail="Wrong password") # se a senha for incorreta: erro 401
-    
-    raise HTTPException(status_code=404, detail="User not found") # se não encontrar o usuário: erro 404
+    user = search_user(form_data.username) # procura um usuário com email igual ao do formulário
+    if not user: # se não encontrar o usuário: erro 404
+        raise HTTPException(status_code=404, detail="User not found")
+    if user.passwd == form_data.password:
+        return TokenResponse(access_token=str(user.id), token_type="bearer") # se encontrar, ele recebe um token equivalente ao seu ID
+    else:
+        raise HTTPException(status_code=401, detail="Wrong password") # se a senha for incorreta: erro 401
 
 
 # EXEMPLO DE UMA FUNÇÃO QUE DEPENDE DO USUÁRIO ESTAR LOGADO:
