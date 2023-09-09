bdbf296f17192a240c28ee227fbe2ebb2356bc88
Hugo Marques <43884516+hggmarks@users.noreply.github.com>
2023-09-09 18:08:25 -0300

    feat: user subscribe page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import CreateTest from "./app/home/pages/CreateTest";
 import ListTests from "./app/home/pages/ListTests";
+import Home from "./app/home/pages/Home";
+import Subscribe from "./app/home/pages/Subscribe";
 
 const router = createBrowserRouter([
   {
     path: "*",
-    Component: CreateTest,
+    Component: Home,
+  },
+  {
+    path: "/users",
+    Component: Subscribe,
   },
   {
     path: "/create-test",
