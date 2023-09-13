86cd1b0b2e6b99d826ccc26ae0b29b61c612b685
Rubens Lima <rubensndelima@hotmail.com>
2023-09-12 23:10:57 -0300

    fix: new get history

--- backend/src/api/history.py
+++ backend/src/api/history.py
@@ -3,44 +3,27 @@ from src.schemas.user import UserDB
 from pydantic import BaseModel
 from typing import Annotated, List
 from src.api.login import get_logged_user
-from src.schemas.videos import VideoModel, MovieModel, MovieList, SerieModel, VideoTime, VideoTimeList, history_db, movies_db, series_db, history_series_db, get_video, getSerieDict, VideoModelAPI, getVideoDataAPI, VideoInfoPost
+from src.schemas.videos import  VideoTimeList, history_db,  VideoAPIList, VideoModelAPI, getVideoDataAPI, VideoInfoPost
 from datetime import datetime
 import requests
+import datetime as dt
+
+def getVideoDataAPI(video_info:VideoInfoPost)->tuple[bool, VideoModelAPI|None]:
+    if video_info.video_type == "movie":
+        url = f"https://api.themoviedb.org/3/movie/{video_info.movie_id}?language=pt-BR"
+    else:
+        url = f"https://api.themoviedb.org/3/tv/{video_info.serie_id}/season/{video_info.season}/episode/{video_info.episode}?language=pt-BR"
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzRhZWEzNDk4MTM4MTYxZjdjMTc1Mzk3YmM1YTgzNiIsInN1YiI6IjY0ZmIzNjQxYTI4NGViMDExZDVmNDdiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lPrDEfSG8Cbi4DsH5Q1W1ho1bJvN9_Iw3blu3jE01fU"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        vm = VideoModelAPI(video_type=video_info.video_type, video_values=response.json(), watched_at=str(dt.datetime.now()))
+        return (True, vm)
+    return(False, None)
 
 router = APIRouter()
-# Rotas de POST para quando o usuário clicar no vídeo ele ser adicionado no banco de dados
-# @router.post("/register_access_video/", 
-#              status_code=201, tags=['history'], response_model= VideoModel)
-# async def register_access(n_video_id: int, 
-#                           user: Annotated [UserDB, Depends(get_logged_user)], 
-#                          ):
-#     video, video_type, serie_name, video_id = get_video(n_video_id)
-
-#     url = f"https://api.themoviedb.org/3/movie/{n_video_id}?language=pt-BR"
-#     headers = {
-#         "accept": "application/json",
-#         "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzRhZWEzNDk4MTM4MTYxZjdjMTc1Mzk3YmM1YTgzNiIsInN1YiI6IjY0ZmIzNjQxYTI4NGViMDExZDVmNDdiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lPrDEfSG8Cbi4DsH5Q1W1ho1bJvN9_Iw3blu3jE01fU"
-#     }
-
-#     response = requests.get(url, headers=headers)
-#     print(response.text)
-
-#     if video_type is not None:
-#         # Inserindo no Banco de Dados de Vídeos
-#         video = VideoTime(**video.model_dump(), watched_at=datetime.now())
-#         if (user.id, user.active_profile) not in history_db.keys():
-#             history_db[(user.id, user.active_profile)] = [video]
-#         else:
-#             history_db[(user.id, user.active_profile)].insert(0,video) # Adiciona no inicio da fila
-#         # Inserindo no Banco de Dados de Series do Usuário
-#         if video_type == 'serie':
-#             if (user.id, serie_name) not in history_series_db.keys():
-#                 history_series_db[((user.id, user.active_profile), serie_name)] = [video_id]
-#             else:
-#                 history_series_db[((user.id, user.active_profile), serie_name)].insert(0,video_id)
-#         return video
-#     else:
-#         raise HTTPException(status_code=404, detail='Não existe esse filme no Banco de Dados da Plataforma')
 
 @router.post("/register_access_video/", 
             status_code=201, tags=['history'], response_model= VideoModelAPI)
@@ -50,7 +33,7 @@ async def register_access(video_info:VideoInfoPost,
     response, model = getVideoDataAPI(video_info)
     if response:
         if (user.id, user.active_profile) not in history_db.keys():
-            history_db[(user.id, user.active_profile)] = {'movie':[], 'serie' : []}        
+            history_db[(user.id, user.active_profile)] = {'movie':[], 'tv' : []}        
         history_db[(user.id, user.active_profile)][model.video_type].insert(0,model)
         return model
     else:
@@ -58,41 +41,51 @@ async def register_access(video_info:VideoInfoPost,
     
 
 # Rota para GET para exibir o histórico todo e mais um para trazer só os dos 10 primeiros
-@router.get("/",status_code=200, response_model=VideoTimeList)
+@router.get("/",status_code=200, response_model=VideoAPIList)
 async def get_history(user: Annotated [UserDB, Depends (get_logged_user)]):
-    return {'videos' : history_db.get((user.id, user.active_profile), [])}
+    try:
+        videos = history_db[(user.id, user.active_profile)].get('movie', []) + history_db[(user.id, user.active_profile)].get('tv', [])
+        videos.sort(key=lambda x: x.watched_at, reverse=True)
+        return {'videos' : videos}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=404, detail= "Você não possui filme e séries no banco de dados")
 
 # Rota para GET para exibir o histórico de apenas 10 vídeos
-@router.get("/only10/",status_code=200, response_model=VideoTimeList)
+@router.get("/only10/",status_code=200, response_model=VideoAPIList)
 async def get_history(user: Annotated [UserDB, Depends (get_logged_user)], init:int=0, final:int=10):
-    videos_h =  history_db.get((user.id, user.active_profile), [])
-    videos_h = [videos_h[i] for i in range(len(videos_h)) if  init<=i < final]
-    return {'videos' :videos_h}
+    try:
+        videos = history_db[(user.id, user.active_profile)].get('movie', [])
+        videos = [videos[i] for i in range(len(videos)) if  init<=i < final]
+        return {'videos' : videos}
+    except :
+        raise HTTPException(status_code=404, detail= "Você não possui filme e séries no banco de dados")
 
 # Rota para GET para exibir o histórico de Filmes
-@router.get("/movies/",status_code=200, response_model=MovieList)
+@router.get("/movies/",status_code=200, response_model=VideoAPIList)
 async def get_history(user: Annotated [UserDB, Depends (get_logged_user)]):
-    movies_h =  history_db.get((user.id, user.active_profile), [])
-    movies_h = [video.id for video in movies_h]
-    watched_mv = [movie for movie in movies_db if movie.id in movies_h]
-    return {'movies' :watched_mv}
+    try:
+        videos = history_db[(user.id, user.active_profile)].get('movie', [])
+        return {'videos' : videos}
+    except:
+        raise HTTPException(status_code=404, detail= "Você não possui filme e séries no banco de dados")
 
-# Rota para GET para exibir um ceto filme procurado
-@router.get("/movies/{movie_name}/",status_code=200, response_model=MovieModel)
+# Rota para GET para exibir um certo filme procurado
+@router.get("/movies/{movie_name}/",status_code=200, response_model=VideoModelAPI)
 async def get_history(user: Annotated [UserDB, Depends (get_logged_user)], 
                       movie_name : str):
     movies_h =  history_db.get((user.id, user.active_profile), [])
-    movies_h = [video.id for video in movies_h]
-    watched_mv = [movie for movie in movies_db if movie.id in movies_h and movie.name == movie_name]
+    watched_mv = [movie for movie in movies_h if movie.video_type == "movie" and movie.video_values['title'] == movie_name]
     if len(watched_mv) > 0 :
         return watched_mv[0]
     else:
         raise HTTPException(status_code=404, detail= "Filme não encontrado no histórico")
 
 # Rota para GET oara exibir o Histórico de Séries
-@router.get("/series/",status_code=200, response_model=List[dict])
+@router.get("/series/",status_code=200, response_model=VideoAPIList)
 async def get_history(user: Annotated [UserDB, Depends (get_logged_user)]):
-    watched_sr_n = [k[1] for k in history_series_db if k[0] == (user.id, user.active_profile)]
-    watched_sr = [serie for serie in series_db if serie.name in watched_sr_n]
-    watched_sr_dl = [getSerieDict(serie, (user.id, user.active_profile)) for serie in watched_sr]
-    return watched_sr_dl
+    try:
+        videos = history_db[(user.id, user.active_profile)].get('movie', [])
+        return {'videos' : videos}
+    except:
+        raise HTTPException(status_code=404, detail= "Você não possui filme e séries no banco de dados")
