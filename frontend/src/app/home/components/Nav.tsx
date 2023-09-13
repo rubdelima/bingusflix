4033fc7d588d7e2786b011f7a2862ef7ccc024eb
Felipe Neiva <felipeneivals@gmail.com>
2023-09-12 22:40:29 -0300

    feat: added menu with logout and account buttons

--- frontend/src/app/home/components/Nav.tsx
+++ frontend/src/app/home/components/Nav.tsx
@@ -1,36 +1,65 @@
-import React, {useEffect} from 'react';
-import "./Nav.css";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Nav.css';
 
 function Nav() {
+  const [show, setShow] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
 
-  const [show, setShow] = React.useState(false);
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/home');
+  }
+
+  const handleProfileClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
+    window.addEventListener('scroll', () => {
       setShow(window.scrollY > 100);
     });
   }, []);
 
   return (
-    <div className={`nav-container ${show && "nav-container-black"}`}>
-    <img 
-      className='nav-logo' 
-      src='/imgs/bingusflix_logo.png' 
-      alt='BingusFlix'
-    ></img>
-    <div className='buttons-nav-container'>
-      <button className='buttons-nav'>Em Alta</button>
-      <button className='buttons-nav'>Filmes</button>
-      <button className='buttons-nav'>Séries</button>
-      <button className='buttons-nav'>Últimos Assistidos</button>
-      <button className='buttons-nav'>Histórico</button>
+    <div className={`nav-container ${show && 'nav-container-black'}`}>
+      <img
+        className='nav-logo'
+        src='/imgs/bingusflix_logo.png'
+        alt='BingusFlix'
+      ></img>
+      <div className='buttons-nav-container'>
+        <button className='buttons-nav'>Em Alta</button>
+        <button className='buttons-nav'>Filmes</button>
+        <button className='buttons-nav'>Séries</button>
+        <button className='buttons-nav'>Últimos Assistidos</button>
+        <button className='buttons-nav'>Histórico</button>
+      </div>
+      <div className='profile-click' onClick={handleProfileClick}>
+        <img
+          className='nav-avatar'
+          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPsVAeFlYeYOEUzb3TV1ML91_LPkkFML5lRQcMdr9nQu2CqO-WzT-RLmkM5_cOKvkaBkI&usqp=CAU'
+          alt='User'
+        ></img>
+      </div>
+
+      {isModalOpen && (
+        <div className='modal'>
+          <div className='modal-content'>
+            <span className='close' onClick={closeModal}>
+              X
+            </span>
+            <button className='profile-button' >Conta</button>
+            <button className='profile-button' onClick={handleLogout}>Logout</button>
+          </div>
+        </div>
+      )}
     </div>
-    <img
-      className='nav-avatar'
-      src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPsVAeFlYeYOEUzb3TV1ML91_LPkkFML5lRQcMdr9nQu2CqO-WzT-RLmkM5_cOKvkaBkI&usqp=CAU'
-      alt='User'
-    ></img>
-  </div>
   );
 }
 
